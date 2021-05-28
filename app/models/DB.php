39315dacae39e7b27cbc812edba286dcdd73727e
Alex <52188223+AlexeyD1982@users.noreply.github.com>
2021-05-28 17:07:21 +0700

    Update DB.php

--- app/models/DB.php
+++ app/models/DB.php
@@ -7,8 +7,7 @@ class DB
     public static function getInstance()
     {
         if (self::$_db == null) {
-            self::$_db = new PDO('mysql:host=mysql-170777.srv.hoster.ru;dbname=srv170777_linkcutter', 'srv170777_root', 'jKuT7.1M');
-//            self::$_db = new PDO('mysql:host=localhost:8890;dbname=linkcutter', 'root', 'root');
+            self::$_db = new PDO('mysql:host=localhost:8890;dbname=linkcutter', 'root', 'root');
         }
         return self::$_db;
     }
